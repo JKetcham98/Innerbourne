8ece9d2d5720afaafb7722130b03134cbad3548d
90299352 <90299352@hs-62291-macair.ad.edenpr.org>
2015-05-13 09:48:52 -0500

    Base
    intro

---
+++ Innerbourne.cpp
@@ -0,0 +1,38 @@
+//Title: final.cpp
+//Author: Joshua Ketcham
+//Description: A adventure game
+//may 12 2015
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+using namespace std;
+
+
+int main() 
+{ 
+cout << "This is a game made by Joshua Ketcham and Vageesha Dharmadasa." << endl;
+sleep(3);
+cout << "\nYou wake up in a cold damp dungeon." << endl;
+sleep(2);
+cout << "You find a steel dagger on the ground along with a tattered scrap of paper..." << endl;
+sleep(2);
+cout << "It seems to be a note." << endl;
+sleep(2);
+cout << "You read the note." << endl;
+sleep(2);
+cout << "It says one thing..." << endl;
+sleep(2);
+cout << "\nSurvive." << endl;
+sleep(2);//intro
+
+
+
+
+
+
+  
+ 
+ 
+            
+return 0;            
+}
