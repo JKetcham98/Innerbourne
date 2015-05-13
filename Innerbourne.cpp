5fa625c0f9f55cd2d280710ed9e758bc3655cfe9
90299352 <90299352@hs-62291-macair.home>
2015-05-13 18:03:12 -0500

    made it 10x better

--- Innerbourne.cpp
+++ Innerbourne.cpp
@@ -13,8 +13,9 @@ using namespace std;
 int main() 
 { 	//player stats
 	int health = 100;//player health
-	int steeld = rand() % 5 + 1;//steel dagger
-	
+	int steeld = rand() % 5 + 1;//first weapon
+	float noshield = rand() % .95 + .93; //the starting no shield
+	int buckler = 3;//first shield
 	
 	//interaction
 	string i1 = "0";//first interaction
@@ -23,7 +24,7 @@ int main()
 
 
 	//enemy damage
-	int d1 = rand() % 5 + 1;//Slime(1)
+	int d1 = rand() % 5 + 3;//Slime(1)
 	
 		
 
@@ -39,12 +40,14 @@ cout << "\nYou wake up in a cold, damp crypt." << endl;
 sleep(2);
 cout << "You find a steel dagger on the ground along with a tattered scrap of paper..." << endl;
 sleep(2);
+cout << "You pick up the dagger and look over at the paper" << endl;
+sleep(2);
 cout << "It seems to be a note." << endl;
 sleep(2);
 
 if (health >=1){//makes sure it ends when you die
 
-cout << "Pick it up(y/n)?" << endl;//note interaction
+cout << "Read it up(y/n)?" << endl;//note interaction
 cin >> i1;
 	if (i1 == "y" || i1 == "yes" || i1 == "Yes" || i1 == "Y" ) {
 	cout << "You read the note." << endl;
@@ -71,12 +74,17 @@ cout << "As you head down the path you come across a slimy beast!" << endl;
   	cin >> choice;
   		//start attack
   		if (choice == "1" || choice == "1." || choice == "attack" || choice == "Attack" || choice == "Attack!" || choice == "attack!" || choice == "1.Attack!") {
-  		cout << "\nYou deal " h1 - steeld << " damage!" << endl;
-  		cout << "You take " << d1 << " damage!" << endl;
+  		cout << "\nYou deal " << steeld << " damage!" << endl;//damage dealt statement
+  		h1 -= steeld;//subtract health from enemy
+  		cout << "You take " << d1 << " damage!" << endl;//damage taken
+  		health -= d1;
   		}//end attack
   	
   		//start defend
-  		
+  		else {
+  		cout << "You take " << d1 * noshield<< " damage!" << endl;
+  		health - (d1 * noshield);
+  		 
   		//end defend
   	}//end fight
   
